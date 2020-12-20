879ff733b969265652bdf1090a2ffb12e9ddd4c5
sammed-sankonatti <myofficialtrack@gmail.com>
2020-12-21 00:17:33 +0530

    final part is the only left thing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component{
 
     state ={
         data :{ },
+        country: ''
     }
 
     async componentDidMount(){
@@ -16,12 +17,17 @@ class App extends React.Component{
 
         this.setState({data:fetchedData});
     }
+
+    handleCountryChange= async (country)=>{
+        console.log(country);
+    }
+
     render(){
         const {data} = this.state;
         return(
             <div className={styles.container}>
                     <Cards data={data} />
-                    <CountryPicker />
+                    <CountryPicker handleCountryChange={this.handleCountryChange} />
                     <Chart />
                     
             </div>
