01ba2f1e9da73f6a7dd09048e91d8fb6a4244a40
sammed-sankonatti <myofficialtrack@gmail.com>
2020-12-20 18:45:39 +0530

    successful implementation of CountUp

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 
 import styles from './Cards.module.css';
-
+import CountUp from 'react-countup';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate}}) => {
     if(!confirmed){
@@ -14,7 +14,14 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate}}) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom >Infected</Typography>
-                        <Typography variant="h5"> {confirmed.value} </Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end= {confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />    
+                        </Typography>
                         <Typography color="textSecondary"> REAL DATE</Typography>
                         <Typography variant="body2">Number of active cases from COVID-19</Typography>
                     </CardContent>
