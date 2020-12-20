20ba4e3c4dbe9404e7b3480b4963c2930be15bb2
sammed-sankonatti <myofficialtrack@gmail.com>
2020-12-20 23:57:58 +0530

    the charts are done and now working on countrypicker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core'
+
+import styles from './CountryPicker.module.css';
+
+import {countries} from '../../api';
 
 const CountryPicker = () => {
+
+    const [fetchedCountries, setFetchedCountries]= useState([]);
+
+    useEffect(()=>{
+        const fetchCountries= async ()=>{
+            setFetchedCountries(await countries);
+        }
+
+        fetchCountries();
+    },[setFetchedCountries]);
+
     return (
         <div>
-             <h2> country picker</h2>
+             <FormControl>
+                 <NativeSelect>
+                     <option value="global">Global</option>
+                 </NativeSelect>
+             </FormControl>
         </div>
     )
 }
 
 export default CountryPicker;
+// className={stylesProvider.formControl}
